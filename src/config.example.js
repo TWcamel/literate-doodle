936c15928fce2796676053619f157903dac7dfb1
TWcamel <j0930495159@gmail.com>
2021-01-16 08:17:00 +0800

    config examle updated

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,13 @@
 
 export default {
-    USER_NAME : 'YOUR_NAME',
+    USER_NAME: "YourName",
+    API_TOKEN: "YourToken",
+    USER_AVATAR: `https://avatars1.githubusercontent.com/u/37062662?s=460&u=f18540c67e83ea46bcdaa1c72b75c17aff1d80ce&v=4)`,
+    USER_INFO:{
+        Name: 'GithubGhost',
+        Intro: 'Im a ghost',
+        Uni: '🏫 National Motherland',
+        Email: '📫 you_cant_touch_me@pm.me',
+        Locaiotn: '🌍 Global',
+    } ,
 }
