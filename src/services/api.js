2f1695a71a9bfbcc495ad5323b956c89473c5481
TWcamel <j0930495159@gmail.com>
2021-01-16 08:09:22 +0800

    remove Authorization in header

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,6 @@ import config from '../config.js'
 export default () => {
     return axios.create({
         baseURL: `https://api.github.com/users/${config.USER_NAME}/repos`,
-        headers: {'Authorization': ` token ${config.API_TOKEN}`}
+        // headers: {'Authorization': ` token ${config.API_TOKEN}`}
     })
 }
